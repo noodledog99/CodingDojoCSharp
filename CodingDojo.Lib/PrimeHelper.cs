e2da7536c08bb11b31a57ada186c5d3f563f7a2d
noodledog99 <nerddy.hahaha@hotmail.com>
2020-12-07 10:50:32 +0700

    setup project

---
+++ CodingDojo.Lib/PrimeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingDojo.Lib
+{
+    public class PrimeHelper
+    {
+    }
+
+    public interface IPrimeHelper
+    {
+        bool IsPrime(int number);
+        int GetPrime(int index);
+    }
+}
