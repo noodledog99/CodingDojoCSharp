24d827b34eac2beb40a43ef877896558b0094c87
noodledog99 <nerddy.hahaha@hotmail.com>
2020-12-08 10:29:37 +0700

    complete sum square differnce.

--- CodingDojo.Con/Program.cs
+++ CodingDojo.Con/Program.cs
@@ -7,8 +7,12 @@ namespace CodingDojo.Con
     {
         static void Main(string[] args)
         {
-            var func = new PrimeHelper();
-            System.Console.WriteLine(func.GetPrime(10001));
+            // var func = new PrimeHelper();
+            // System.Console.WriteLine(func.GetPrime(10001));
+
+            var func = new SumSquareDifference();
+            System.Console.WriteLine(func.SquaresOfSum(100));
+            System.Console.WriteLine(func.SumOfSquares(100));
         }
     }
 }
