027a352c3ec2856b1980dec12bac7c92a5dac0d5
noodledog99 <nerddy.hahaha@hotmail.com>
2020-12-10 16:12:44 +0700

    complete power digit sum

--- CodingDojo.Con/Program.cs
+++ CodingDojo.Con/Program.cs
@@ -10,9 +10,19 @@ namespace CodingDojo.Con
             // var func = new PrimeHelper();
             // System.Console.WriteLine(func.GetPrime(10001));
 
-            var func = new SumSquareDifference();
-            System.Console.WriteLine(func.SquaresOfSum(100));
-            System.Console.WriteLine(func.SumOfSquares(100));
+            // var func = new SumSquareDifference();
+            // System.Console.WriteLine(func.SquaresOfSum(100));
+            // System.Console.WriteLine(func.SumOfSquares(100));
+
+            var func = new PowerDigitSum();
+            System.Console.WriteLine(func.SumOfDigit((func.PowerNumber(1000))));
+            
+            // var xx = "367777";
+            // var x = xx[1];
+            // System.Console.WriteLine(x);
+            // System.Console.WriteLine(Convert.ToInt32(x)); // ascii
+            // System.Console.WriteLine(int.Parse(x.ToString())); 
+
         }
     }
 }
